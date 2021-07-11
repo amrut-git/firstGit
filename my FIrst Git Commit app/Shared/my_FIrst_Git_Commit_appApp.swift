658263835f348079804a4e75a437d86b8ef45980
mac <mac@apples-MacBook-Pro-2.local>
2021-07-11 20:11:40 +0530

    Initial Commit on the git

---
+++ my FIrst Git Commit app/Shared/my_FIrst_Git_Commit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_FIrst_Git_Commit_appApp.swift
+//  Shared
+//
+//  Created by mac on 11/07/21.
+//
+
+import SwiftUI
+
+@main
+struct my_FIrst_Git_Commit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
